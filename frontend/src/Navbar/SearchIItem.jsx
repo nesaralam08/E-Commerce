16473e21c73fd44ae5845e8aad73a06afd4a6c30
NESAR ALAM <itsnesar15@gmail.com>
2024-12-11 14:02:01 +0530

    search option modified

--- frontend/src/Navbar/SearchIItem.jsx
+++ frontend/src/Navbar/SearchIItem.jsx
@@ -5,30 +5,36 @@ import { useNavigate } from 'react-router-dom'
 
 function SearchIItem() {
     const [query, setQuery] = useState('')
+    const [loading, setLoading] = useState(false)
     const navigate = useNavigate()
     const handleSearch = async (e) => {
         e.preventDefault();
-
         if (query.trim()) {
             try {
+                setLoading(true)
                 const response = await AxiosInstance.get(`/api/product/search?q=${query}`);
                 const products = response.data.result;
                 navigate('/search-results', { state: { query, products } });
                 setQuery('');
+                setLoading(false)
             } catch (error) {
                 console.error('Error fetching search results:', error);
+                setLoading(false)
             }
         }
     }
-        return (
-            <>
+    return (
+        <>
 
-                <form onClick={handleSearch} className='flex items-center justify-between w-full'>
-                    <input type="text" className="grow text-sm" placeholder="Search for products...." value={query} onChange={(e) => setQuery(e.target.value)} />
-                    <button type='submit'><CiSearch className='text-xl text-slate-600' /></button>
-                </form>
-            </>
-        )
-    }
+            <form onClick={handleSearch} className='flex items-center justify-between w-full'>
+                <input type="text" className="grow text-sm" placeholder="Search for products...." value={query} onChange={(e) => setQuery(e.target.value)} />
 
-    export default SearchIItem
+                {
+                    loading ? <span className="loading loading-spinner loading-sm text-slate-400"></span> : <button type='submit'><CiSearch className='text-xl text-slate-600' /></button>
+                }
+            </form>
+        </>
+    )
+}
+
+export default SearchIItem
